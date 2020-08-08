1dd7f435dc7d80177d60844744a8318e3c4fee55
wikz87 <wikz@live.com>
2020-08-08 11:54:59 +0500

    Added PWA capability

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 // Smooth Scroll behavior polyfill
 // https://github.com/iamdustan/smoothscroll
