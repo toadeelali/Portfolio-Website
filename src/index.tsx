12c92c338f5fcfa3bf1fe87fc31d801ec4c4da30
Adeel Ali <wikz@live.com>
2021-06-06 06:27:33 -0400

    fix: add formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import smoothscroll from 'smoothscroll-polyfill';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles'
+
+import App from './App'
+
+import * as serviceWorker from './serviceWorker'
+import smoothscroll from 'smoothscroll-polyfill'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {' '}
+    <App />{' '}
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register()
 
 // Smooth Scroll behavior polyfill
 // https://github.com/iamdustan/smoothscroll
-smoothscroll.polyfill();
+smoothscroll.polyfill()
